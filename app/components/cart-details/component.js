392e96ad4c5d77ec649887e448e1043d3fa29e9a
Stephanie Gurung <stephr3@gmail.com>
2016-08-26 11:32:36 -0700

    utilize computed property to display average rating with star icons

--- app/components/cart-details/component.js
+++ app/components/cart-details/component.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  ratingAverage: Ember.computed('comments.@each.rating', function(){
+    {{debugger}}
+    var total = 0;
+    (this.get('comments')).forEach(function(comment){
+      total += comment.get('rating');
+    });
+    return Math.round(total/this.get('comments').get('length'));
+  }),
   actions: {
     saveComment(params) {
       this.sendAction('saveComment', params);
