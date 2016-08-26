3dd17e501ef49cdc2e76531237b8aef1293f5926
Stephanie Gurung <stephr3@gmail.com>
2016-08-26 13:35:09 -0700

    allow user to remove carts from favorites list

--- app/components/display-favorites/component.js
+++ app/components/display-favorites/component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  favoritesList: Ember.inject.service()
+  favoritesList: Ember.inject.service(),
+  actions: {
+    remove(item) {
+      this.get('favoritesList').remove(item);
+    }
+  }
 });
