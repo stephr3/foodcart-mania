26bd565c8263007c1e10dc693f64a08e6b03bf06
Stephanie Gurung <stephr3@gmail.com>
2016-08-26 11:37:43 -0700

    display average rating with star icons on index route

--- app/components/cart-tile/component.js
+++ app/components/cart-tile/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  ratingAverage: Ember.computed('comments.@each.rating', function(){
+    var total = 0;
+    (this.get('comments')).forEach(function(comment){
+      total += comment.get('rating')
+    });
+    return Math.round(total/this.get('comments').get('length'));
+  })
 });
