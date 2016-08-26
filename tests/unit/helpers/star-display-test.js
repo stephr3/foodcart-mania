d98d7e5f3ab3a1209fa3186f8a4b01e43d8212ac
Stephanie Gurung <stephr3@gmail.com>
2016-08-26 11:20:29 -0700

    utilize star display helper to display ratings as star icons

---
+++ tests/unit/helpers/star-display-test.js
@@ -0,0 +1,10 @@
+import { starDisplay } from 'foodcart-mania/helpers/star-display';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | star display');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = starDisplay([42]);
+  assert.ok(result);
+});
