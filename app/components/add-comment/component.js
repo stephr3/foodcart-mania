8a294d3ec386416e1ab85caf4e85c935962297c4
Stephanie Gurung <stephr3@gmail.com>
2016-08-26 10:44:41 -0700

    allow user to save a comment to a food cart page

---
+++ app/components/add-comment/component.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  ratings: ["*", "**", "***", "****", "*****"],
+  addNewComment: false,
+  actions: {
+    addNewComment(){
+      this.set('addNewComment', true);
+    },
+    saveComment(){
+      var params = {
+        rating: this.get('rating'),
+        text: this.get('text'),
+        author: this.get('author'),
+        date: Date.now(),
+        cart: this.get('cart')
+      };
+      this.sendAction('saveComment', params);
+      this.set('addNewComment', false);
+      this.set('author', "");
+      this.set('rating', "");
+      this.set('text', "");
+    }
+  }
+});
--- app/components/cart-details/component.js
+++ app/components/cart-details/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    saveComment(params) {
+      this.sendAction('saveComment', params);
+    }
+  }
 });
