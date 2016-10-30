f5ce66b9b61cb1743741a08ac6c8892867e13d43
Stephanie Roth <stephr3@gmail.com>
2016-10-30 15:16:24 -0700

    prepare for gh pages deployment

--- app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  favoritesList: Ember.inject.service(),
-  favoritesTotalDisplay: Ember.computed('favoritesList.items.[]', function(){
-    return this.get('favoritesList').get('items').get('length');
-  })
-});
--- app/models/comment.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  author: DS.attr(),
-  date: DS.attr(),
-  rating: DS.attr(),
-  text: DS.attr(),
-  cart: DS.belongsTo('cart', {async:true})
-});
